9cc46341ebe42b57a215360945c445d2d663b021
Luca Chilan <lucachilan@gmail.com>
2026-09-15 17:13:45 -0300

    team act

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -11,6 +11,7 @@
  * Each row represents the player's stats for a single season with a single team.
  */
 
+using System.ComponentModel;
 using Microsoft.VisualBasic.FileIO;
 
 public class Basketball
@@ -27,10 +28,24 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+            if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            }
+            else
+            {
+                players[playerId] = points;
+            }
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        var topPlayers = players.ToArray();
+        Array.Sort(topPlayers, (p1, p2)=> p2.Value - p1.Value);
 
-        var topPlayers = new string[10];
+        for (var i=0; i<10; i++)
+        {
+            Console.WriteLine(topPlayers[i]);
+        }
+
+
     }
 }
--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,4 +1,6 @@
-﻿public static class DisplaySums {
+﻿using System.Globalization;
+
+public static class DisplaySums {
     public static void Run() {
         DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         // Should show something like (order does not matter):
@@ -29,5 +31,15 @@
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+        var sum = new HashSet<int>();
+
+        foreach (var number in numbers)
+        {
+            if (sum.Contains(10 - number))
+            {
+                Console.WriteLine($"{number} {10-number}");
+            }
+            sum.Add(number);
+        }
     }
 }
